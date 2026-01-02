8b6b8f22d9b78c1a4600fc3db21550cfe7f3ed5d
Edhar Karpovich <knzmt3@yandex.ru>
2026-01-02 21:58:16 +0300

    add lab11

---
+++ WpfPart1/Lab1_FirstWPFApp_stnk/ManyWindows_exercise_5/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ManyWindows_exercise_5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly DateTime _designTime = new DateTime(2026, 01, 02, 14, 30, 0);
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpMBoxButton_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime runTime = DateTime.Now;
+
+            string designTimeFormatted = _designTime.ToString("dd, MMMM, yyyy, HH:mm");
+            string runTimeFormatted = runTime.ToString("dd, MMMM, yyyy, HH:mm");
+
+            string message = $"Привет из WPF приложения с графическим интерфейсом. " +
+                           $"Автор: Карпович Эдгар, группа Пв1-24ПО, " +
+                           $"Время создания = {designTimeFormatted}, " +
+                           $"Время запуска приложения = {runTimeFormatted}";
+
+            MessageBox.Show(message, "Приветствие", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void OpWinButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 win1 = new Window1();
+            win1.Show();
+
+        }
+    }
+}
---
+++ WpfPart1/Lab1_FirstWPFApp_stnk/lab11_task1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab11_task1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        { // круг для иконки главного окна
+            InitializeComponent();
+            var circle = new EllipseGeometry(new Point(16, 16), 12, 12);
+            var drawing = new GeometryDrawing(Brushes.Red, null, circle);
+            this.Icon = new DrawingImage(drawing);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime runTime = DateTime.Now;
+
+            string runTimeFormatted = runTime.ToString("dd.MM.yyyy HH:mm");
+
+            string message = $"Привет из WPF приложения с графическим интерфейсом. " +
+                           $"Автор: Карпович Эдгар, группа Пв1-24ПО. " +
+                           $"Время запуска приложения = {runTimeFormatted}";
+            // настройка окна сообщения
+            MessageBox.Show(message, "Приветствие", MessageBoxButton.YesNoCancel, MessageBoxImage.Warning, MessageBoxResult.Cancel);
+        }
+    }
+}
